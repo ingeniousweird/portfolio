3e2bf9f0f7550cf2be8eb019d5dabbc3de863a63
ingeniousweird <69917945+ingeniousweird@users.noreply.github.com>
2023-07-26 13:22:50 -0400

    Add files via upload

---
+++ app/components/home/education/education/education.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.scss']
+})
+export class EducationComponent implements OnInit {
+
+  active = 0
+  
+  constructor(
+    public analyticsService: AnalyticsService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
